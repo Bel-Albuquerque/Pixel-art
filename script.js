6d455a8844326bade1c44df38b01e043771ec8aa
Bel Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-07-08 15:54:24 -0300

    requisito 9

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let yellow = document.querySelector('.amarelo')
 let colorYellow = yellow.style.backgroundColor = 'yellow'
 let palette = document.querySelector('.palette')
 let pixelBoard = document.querySelector('#pixel-board')
+let pixel = document.querySelectorAll('.pixel')
 
 
 function colorselect (event) {
@@ -31,8 +32,30 @@ function pixelColor (event) {
   let colorSelectFundo =colorSelect.style.backgroundColor
   pixelSelect.style.backgroundColor = colorSelectFundo
 }
-let h1 = document.querySelector('h1')
-
 pixelBoard.addEventListener('click', pixelColor)
 
+
+function buttonLimparJogo (buttonName) {
+  let buttonPai = document.querySelector('#button')
+  let buttonFilho = document.createElement('button')
+  let buttonId = 'buttonSelect'
+
+  buttonFilho.innerHTML = buttonName
+  buttonFilho.id = buttonId
+  buttonPai.appendChild(buttonFilho) 
+ }
+buttonLimparJogo("Limpar Jogo")
+
+let button = document.querySelector('#buttonSelect')
+
+function limparJogo (event) {
+ let colorSelect = document.querySelector('.selected')
+ colorSelect.classList.remove('selected')
+  black.classList.add('selected')
+  for (let index = 0; index < pixel.length; index += 1) {
+    let quadradinho = pixel[index]
+    quadradinho.style.backgroundColor = 'white'
+  }
+}
+button.addEventListener('click', limparJogo)
 
